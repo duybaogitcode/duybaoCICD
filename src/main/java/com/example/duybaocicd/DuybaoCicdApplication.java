926d5315b40e6e2a43761706e5953d989999f4ab
Duy Bao <bao1322002@gmail.com>
2023-05-21 16:57:32 +0700

    first commit

---
+++ src/main/java/com/example/duybaocicd/DuybaoCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.duybaocicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuybaoCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuybaoCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/duybaocicd/DuybaoCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duybaocicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuybaoCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
